3db0c014852de66b5e512f3e2d989ed0c57de45b
Mohammed Tangestani <tangestani@gmail.com>
2014-07-06 20:25:20 -0400

    Initial commit of Parse CloudCode

---
+++ CloudCode/cloud/main.js
@@ -0,0 +1,35 @@
+
+Parse.Cloud.define("closestVybes", function(request, response) {
+  var userGeoPoint = request.params.location;
+  var query = new Parse.Query("Vybe");
+  query.near("location", userGeoPoint);
+  query.limit(10);
+  query.find({
+    success: function(vybesObjects) {
+      response.success(vybesObjects);
+    },
+    error: function() {
+      response.error("cannot find vybes around you");
+    }
+  });
+});
+
+Parse.Cloud.afterSave("Vybe", function(request, response) {
+  var userGeoPoint = request.object.get("location");
+  console.info("userGeoPoint: " + userGeoPoint);
+
+  var query = new Parse.Query("Vybe");
+  console.info("query: " + query);
+
+  query.near("location", userGeoPoint);
+  query.limit(10);
+  query.find({
+    success: function(vybesObjects) {
+      console.info("Returning responses: " + vybesObjects);
+      response.success(vybesObjects);
+    },
+    error: function(error) {
+      console.error("Got an error " + error.code + " : " + error.message);
+    }
+  });
+});
