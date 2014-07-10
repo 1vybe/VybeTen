ded44665875187974c5537d069dadcf6b8357456
Mohammed Tangestani <tangestani@gmail.com>
2014-07-10 04:25:16 -0400

    Add currying utility function

---
+++ CloudCode/cloud/utils.js
@@ -0,0 +1,9 @@
+// Partially evaluate a function
+exports.curry = function(fn) {
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  return function() {
+    return fn.apply(this, args.concat(
+      Array.prototype.slice.call(arguments, 0)));
+  };
+};
