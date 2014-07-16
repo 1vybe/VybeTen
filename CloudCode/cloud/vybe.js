51f4738d813f62821f4efc2dc0e47a7561bcb57a
Mohammed Tangestani <tangestani@gmail.com>
2014-07-16 03:17:27 -0400

    Move Vybe object code to vybe.js

---
+++ CloudCode/cloud/vybe.js
@@ -0,0 +1,82 @@
+// Validate Vybes have a valid owner in the "user" pointer.
+Parse.Cloud.beforeSave('Vybe', function(request, response) {
+  var currentUser = request.user;
+  var objectUser = request.object.get('user');
+
+  if(!currentUser || !objectUser) {
+    response.error('A Vybe should have a valid user.');
+  } else if (currentUser.id === objectUser.id) {
+    response.success();
+  } else {
+    response.error('Cannot set user on Vybe to a user other than the current user.');
+  }
+});
+
+
+// recentVybesInReverse
+var algorithm1 = get_vybes.bind(
+  this, {
+    recent: true,
+    reversed: true,
+  });
+
+// recentVybesExcludingYouInReverse
+var algorithm2 = get_vybes.bind(
+  this, {
+    recent: true,
+    reversed: true,
+    hide_user: true,
+  });
+
+// recentNearbyVybesExcludingYou
+var algorithm3 = get_vybes.bind(
+  this, {
+    recent: true,
+    nearby: true,
+    hide_user: true,
+  });
+
+
+// Default algorithm used in the app
+Parse.Cloud.define("default_algorithm", algorithm1);
+
+// Algorithms that can be chosen from the debug menu
+Parse.Cloud.define("algorithm1", algorithm1);
+Parse.Cloud.define("algorithm2", algorithm2);
+Parse.Cloud.define("algorithm3", algorithm3);
+
+
+// Generic get_vybes functions that accepts options
+function get_vybes(options, request, response) {
+  var recent = options.recent || false;
+  var nearby = options.nearby || false;
+  var hide_user = options.hide_user || false;
+  var reversed = options.reversed || false;
+  var limit = options.limit || 10;
+
+  var userGeoPoint = request.params.location;
+  var currentUser = Parse.User.current();
+
+  var query = new Parse.Query("Vybe");
+
+  if (recent) query.addDescending("timestamp");
+  if (nearby) query.near("location", userGeoPoint);
+  if (hide_user) query.notEqualTo("user", currentUser);
+  if (limit) query.limit(limit);
+
+  query.find({
+    success: function(vybesObjects) {
+      var result;
+      if (reversed) {
+        result = vybesObjects;
+      } else {
+        // Sort result in chronological order
+        result = vybesObjects.reverse();
+      }
+      response.success(result);
+    },
+    error: function() {
+      response.error("cannot find vybes around you");
+    }
+  });
+}
