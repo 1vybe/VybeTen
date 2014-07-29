9811a47f45c0b918f2faf86578fe32e86a781d16
Mohammed Tangestani <tangestani@gmail.com>
2014-07-29 11:16:45 -0400

    Send a push notification for private vybes

--- CloudCode/cloud/vybe.js
+++ CloudCode/cloud/vybe.js
@@ -2,7 +2,7 @@
 Parse.Cloud.beforeSave('Vybe', function(request, response) {
   var currentUser = request.user;
   var objectUser = request.object.get('user');
- 
+
   if(!currentUser || !objectUser) {
     response.error('A Vybe should have a valid user.');
   } else if (currentUser.id === objectUser.id) {
@@ -11,21 +11,27 @@ Parse.Cloud.beforeSave('Vybe', function(request, response) {
     response.error('Cannot set user on Vybe to a user other than the current user.');
   }
 });
- 
+
 // Sends a Yo to all who Yo VybeDev
 Parse.Cloud.afterSave('Vybe', function(request) {
   // Only send push notifications for new vybes
   if (request.object.existed()) {
     return;
   }
- 
-  if (request.object.get('isPublic')) {
-    sendYo();
-  } else {
+
+  // Send iOS push notifications to tribe members
+  if (request.user.has('tribe')) {
+    console.log("This dude's got a tribe.");
     sendPush(request);
   }
+
+  // Send Yo's for public vybes
+  if (request.object.get('isPublic')) {
+    console.log("This vybe is public. Sending a Yo.");
+    sendYo();
+  }
 });
- 
+
 var sendYo = function() {
   Parse.Cloud.httpRequest({
     method: 'POST',
@@ -41,19 +47,21 @@ var sendYo = function() {
     }
   });
 };
- 
+
 var sendPush = function(request) {
   var user = request.object.get("user");
   if (!user) {
     throw "Undefined user. Skipping push for Vybe " +
-      request.object.id;
+          request.object.id;
   }
- 
+
   var currentUser = Parse.User.current();
- 
+
+  var query = new Parse.Query(Parse.User);
+
   var relation = currentUser.get('tribe').relation('members');
   relation.query().find().then(function(members) {
- 
+
     var query = new Parse.Query(Parse.Installation);
     query.containedIn('user', members);
     Parse.Push.send({
@@ -67,67 +75,67 @@ var sendPush = function(request) {
     });
   });
 };
- 
+
 var alertMessage = function(request) {
   var message = "";
- 
+
   message = "Someone in your tribe posted a vybe.";
- 
+
   // Trim our message to 140 characters.
   if (message.length > 140) {
     message = message.substring(0, 140);
   }
- 
+
   return message;
 };
- 
+
 var alertPayload = function(request) {
   var payload = {};
   return {
     alert: alertMessage(request), // Set our alert message.
     sound: '',
     badge: 'Increment', // Increment the target device's badge count.
-    // vid: request.object.id // Vybe Id
+    vid: request.object.id, // Vybe Id
   };
 };
- 
+
 // Default algorithm used in the app
 Parse.Cloud.define('default_algorithm',
-		   get_vybes.bind(this, {
-		     recent: true,
-		     reversed: true,
-		   })
-		  );
- 
+  get_vybes.bind(this, {
+    recent: true,
+    reversed: true,
+  })
+);
+
 // Algorithms that can be chosen from the debug menu
 Parse.Cloud.define('algorithm1',
-		   get_vybes.bind(this, {
-		     recent: true,
-		     reversed: true,
-		   })
-		  );
+  get_vybes.bind(this, {
+    recent: true,
+    reversed: true,
+  })
+);
 Parse.Cloud.define('algorithm2',
-		   get_vybes.bind(this, {
-		     recent: true,
-		     reversed: true,
-		     hide_user: true,
-		   })
-		  );
+  get_vybes.bind(this, {
+    recent: true,
+    reversed: true,
+    hide_user: true,
+  })
+);
 Parse.Cloud.define('algorithm3',
-		   get_vybes.bind(this, {
-		     recent: true,
-		     nearby: true,
-		     hide_user: true,
-		   })
-		  );
- 
+  get_vybes.bind(this, {
+    recent: true,
+    nearby: true,
+    hide_user: true,
+  })
+);
+
 Parse.Cloud.define('get_tribe_vybes',
-		   get_tribe_vybes.bind(this, {
-		     recent: true,
-		     reversed: true,
-		   })
-		  );
- 
+  get_tribe_vybes.bind(this, {
+    recent: true,
+    reversed: true,
+  })
+);
+
 // Generic get_vybes functions that accepts options
 function get_vybes(options, request, response) {
   var recent = options.recent || false;
@@ -135,12 +143,12 @@ function get_vybes(options, request, response) {
   var hide_user = options.hide_user || false;
   var reversed = options.reversed || false;
   var limit = options.limit || 10;
- 
+
   var userGeoPoint = request.params.location;
   var currentUser = Parse.User.current();
- 
+
   var query = new Parse.Query('Vybe');
- 
+
   if (recent)
     query.addDescending('timestamp');
   if (nearby)
@@ -149,10 +157,10 @@ function get_vybes(options, request, response) {
     query.notEqualTo('user', currentUser);
   if (limit)
     query.limit(limit);
- 
+
   // Don't get private vybes
   query.notEqualTo('isPublic', false);
- 
+
   query.find({
     success: function(vybesObjects) {
       if (reversed) {
@@ -167,7 +175,7 @@ function get_vybes(options, request, response) {
     }
   });
 }
- 
+
 // Generic get_tribe_vybes functions that accepts options
 function get_tribe_vybes(options, request, response) {
   var recent = options.recent || false;
@@ -175,22 +183,22 @@ function get_tribe_vybes(options, request, response) {
   var hide_user = options.hide_user || false;
   var reversed = options.reversed || false;
   var limit = options.limit || 10;
- 
+
   var userGeoPoint = request.params.location;
   var currentUser = Parse.User.current();
- 
+
   if (!currentUser.get('tribe')) {
     response.error('User does not belong to a tribe.');
   }
- 
+
   var relation = currentUser.get('tribe').relation('members');
   var query = new Parse.Query(Parse.User);
   relation.query().find().then(function(members) {
- 
+
     var query = new Parse.Query('Vybe');
- 
+
     query.containedIn('user', members);
- 
+
     if (recent)
       query.addDescending('timestamp');
     if (nearby)
@@ -199,7 +207,7 @@ function get_tribe_vybes(options, request, response) {
       query.notEqualTo('user', currentUser);
     if (limit)
       query.limit(limit);
- 
+
     query.find({
       success: function(vybesObjects) {
         if (reversed) {
