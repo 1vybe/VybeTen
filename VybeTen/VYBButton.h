322a593e5373541a760743fb6aa8485c68d2708c
jinsukim <jinsukim21@gmail.com>
2014-10-28 14:13:49 -0400

    activity button on capture screen has two sets (normal/highlighted) of images for active and non-active states. custom class VYBButton must call setActiveImage: and setNormalImage to set up this

---
+++ VybeTen/VYBButton.h
@@ -0,0 +1,13 @@
+//
+//  VYBButton.h
+//  VybeTen
+//
+//  Created by jinsuk on 10/28/14.
+//  Copyright (c) 2014 Vybe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VYBButton : UIButton
+
+@end
---
+++ VybeTen/VYBButton.m
@@ -0,0 +1,13 @@
+//
+//  VYBButton.m
+//  VybeTen
+//
+//  Created by jinsuk on 10/28/14.
+//  Copyright (c) 2014 Vybe. All rights reserved.
+//
+
+#import "VYBButton.h"
+
+@implementation VYBButton
+
+@end
