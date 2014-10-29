a1656ceedb01f3ad7968919c570a3cdd6f624dc9
Mohammed Tangestani <tangestani@gmail.com>
2014-10-29 02:08:51 -0400

    Hook up the VybeCell counter labels in ActivityVC

--- VybeTen/VYBVybeTableViewCell.h
+++ VybeTen/VYBVybeTableViewCell.h
@@ -12,6 +12,8 @@
 
 @property (nonatomic, weak) IBOutlet UILabel *timestampLabel;
 @property (nonatomic, weak) IBOutlet UILabel *locationLabel;
+@property (nonatomic, weak) IBOutlet UILabel *nearbyCountLabel;
+@property (nonatomic, weak) IBOutlet UILabel *viewCountLabel;
 @property (nonatomic, weak) IBOutlet PFImageView *thumbnailImageView;
 
 @end
