b6979fb4ce61ae7df4839d4e5ce611fa74f216b4
jinsukim <jinsukim21@gmail.com>
2015-01-23 20:21:27 -0500

    SimpleTabBarView responds to user interaction

--- VybeTen/SimpleTabBarController.swift
+++ VybeTen/SimpleTabBarController.swift
@@ -8,8 +8,41 @@
 
 import UIKit
 
+class SimpleTabBarView: UIView {
+  @IBOutlet weak var homeButton: UIButton!
+  @IBOutlet weak var discoverButton: UIButton!
+  @IBOutlet weak var activityButton: UIButton!
+  @IBOutlet weak var profileButton: UIButton!
+  weak var selectedButton: UIButton?
+
+  func selectButton(button: UIButton) {
+    if !button.selected {
+      selectedButton?.selected = false
+      button.selected = true
+      selectedButton = button
+    }
+  }
+  
+  func selectHomeTab() {
+    self.selectButton(homeButton)
+  }
+
+  func selectDiscoverTab() {
+    self.selectButton(discoverButton)
+  }
+  
+  func selectActivityTab() {
+    self.selectButton(activityButton)
+  }
+  
+  func selectProfileTab() {
+    self.selectButton(profileButton)
+  }
+}
+
 class SimpleTabBarController: UIViewController {
-  @IBOutlet weak var tabBar: UIView!
+  @IBOutlet weak var tabBar: SimpleTabBarView!
+  
   @IBOutlet weak var containerView: UIView!
   
   var mainNavigationController: VYBNavigationController?
@@ -19,10 +52,31 @@ class SimpleTabBarController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    tabBar.selectHomeTab()
+  }
+  
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    if segue.identifier == "TabBarEmbedSegue" {
-      if let destViewController = segue.destinationViewController as? VYBNavigationController {
-        mainNavigationController = destViewController
+    if let identifier = segue.identifier {
+      if identifier == "TabBarEmbedSegue" {
+        if let destViewController = segue.destinationViewController as? VYBNavigationController {
+          mainNavigationController = destViewController
+        }
+      } else {
+        switch identifier {
+        case "HomeTabSegue":
+          tabBar.selectHomeTab()
+        case "DiscoverTabSegue":
+          tabBar.selectDiscoverTab()
+        case "ActivityTabSegue":
+          tabBar.selectActivityTab()
+        case "ProfileTabSegue":
+          tabBar.selectProfileTab()
+        default:
+          return
+        }
       }
     }
   }
@@ -30,15 +84,5 @@ class SimpleTabBarController: UIViewController {
   func presentTabbedViewController(viewController: UIViewController) {
     mainNavigationController?.setViewControllers([viewController], animated:true)
   }
-  
-  /*
-  // MARK: - Navigation
-  
-  // In a storyboard-based application, you will often want to do a little preparation before navigation
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-  // Get the new view controller using segue.destinationViewController.
-  // Pass the selected object to the new view controller.
-  }
-  */
-  
+
 }
---
+++ VybeTen/VybeCardCell.swift
@@ -0,0 +1,13 @@
+//
+//  VybeCardCell.swift
+//  VybeTen
+//
+//  Created by Jinsu Kim on 1/23/15.
+//  Copyright (c) 2015 Vybe. All rights reserved.
+//
+
+import UIKit
+
+class VybeCardCell: UICollectionViewCell {
+    
+}
