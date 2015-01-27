2a0a21cfc7b92e4405e85bb2dac1d5298c26824d
jinsukim <jinsukim21@gmail.com>
2015-01-27 17:10:12 -0500

    Category for UIStoryboard to manage in one place all functions calls that instantiate storyboards

---
+++ VybeTen/UIStoryboard+Vybe.h
@@ -0,0 +1,13 @@
+//
+//  UIStoryboard+Vybe.h
+//  VybeTen
+//
+//  Created by Jinsu Kim on 1/26/15.
+//  Copyright (c) 2015 Vybe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIStoryboard (Vybe)
++ (UIStoryboard *)homeStoryboard;
+@end
---
+++ VybeTen/UIStoryboard+Vybe.m
@@ -0,0 +1,17 @@
+//
+//  UIStoryboard+Vybe.m
+//  VybeTen
+//
+//  Created by Jinsu Kim on 1/26/15.
+//  Copyright (c) 2015 Vybe. All rights reserved.
+//
+
+#import "UIStoryboard+Vybe.h"
+
+@implementation UIStoryboard (Vybe)
+
++ (UIStoryboard *)homeStoryboard {
+  return [UIStoryboard storyboardWithName:@"Home" bundle:[NSBundle mainBundle]];
+}
+
+@end
