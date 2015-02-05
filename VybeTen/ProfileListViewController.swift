01b953c782aa4b15921a3c25a2c095c66b3f18c0
jinsukim <jinsukim21@gmail.com>
2015-02-05 17:48:56 -0500

    Subclass UICollection instead of PFQueryCollection for ProfileCollectionVC

--- VybeTen/ProfileListViewController.swift
+++ VybeTen/ProfileListViewController.swift
@@ -22,16 +22,14 @@ class ProfileListViewController: PFQueryTableViewController {
    
     println("My Vybes Table viewDidLoad")
     
+    tableView.contentInset = UIEdgeInsetsMake(100.0, 0.0, 0.0, 0.0)
+    
     tableView.estimatedRowHeight = 300.0
     tableView.rowHeight = UITableViewAutomaticDimension
     
     tableView.tableFooterView = UIView(frame: CGRectZero)
     
     // Do any additional setup after loading the view.
-  }
-  
-  override func objectsDidLoad(error: NSError!) {
-    super.objectsDidLoad(error)
   }
   
   override func queryForTable() -> PFQuery! {
@@ -44,24 +42,37 @@ class ProfileListViewController: PFQueryTableViewController {
   }
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return objects.count + 1
+    return objects.count
+  }
+  
+  override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return 0.0
+  }
+  
+  override func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+    return 0.0
   }
   
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    if indexPath.row == 0 {
-      return tableView.dequeueReusableCellWithIdentifier("EmptyListCell") as UITableViewCell
-    }
+//    if indexPath.row == 0 {
+//      return tableView.dequeueReusableCellWithIdentifier("EmptyListCell") as UITableViewCell
+//    }
     
     let cell = tableView.dequeueReusableCellWithIdentifier("ListVybeCardCell") as VybeCardCell
     
-    if let vybeObj = objects[indexPath.row - 1] as? PFObject {
+    if let vybeObj = objects[indexPath.row] as? PFObject {
       if let thumbnailFile = vybeObj.objectForKey(kVYBVybeThumbnailKey) as? PFFile {
         cell.thumbnailImageView.file = thumbnailFile
         cell.thumbnailImageView.loadInBackground({ (image: UIImage!, error: NSError!) -> Void in
           if image != nil {
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-              cell.thumbnailImageView.image = image
-            })
+            var maskImage: UIImage?
+            if image.size.height > image.size.width {
+              // Portrait
+              maskImage = UIImage(named: "Mask_P")
+            } else {
+              maskImage = UIImage(named: "Mask_L")
+            }
+            cell.thumbnailImageView.image = VYBUtility.maskImage(image, withMask: maskImage)
           }
         })
       }
