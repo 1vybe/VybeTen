6def2ed17815d640251082ce1d6ef21afff02602
jinsukim <jinsukim21@gmail.com>
2015-02-20 18:12:04 -0500

    Fix collection cell size bug in Tribes screen

---
+++ VybeTen/TribeCollectionCell.swift
@@ -0,0 +1,15 @@
+//
+//  TribeCollectionCell.swift
+//  Vybe
+//
+//  Created by Jinsu Kim on 2/20/15.
+//  Copyright (c) 2015 Vybe. All rights reserved.
+//
+
+import UIKit
+
+class TribeCollectionCell: UICollectionViewCell {
+  @IBOutlet weak var photoImageView: PFImageView!
+  @IBOutlet weak var nameLabel: UILabel!
+  
+}
